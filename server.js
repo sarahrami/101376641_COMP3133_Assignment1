3c8cb2a0d1226e353bec9b5a2c3c67b7d3807d55
sarahrami <sarahrami97@gmail.com>
2024-04-07 00:11:07 -0400

    async function

--- server.js
+++ server.js
@@ -8,33 +8,36 @@ const cors = require('cors');
 const schema = require("./graphql/Schema");
 const root = require("./graphql/resolvers");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+async function startServer() {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-const MONGODB_URI = "mongodb+srv://sarahrami97:rKcXzEa3p234lbya@cluster0.umsf83y.mongodb.net/comp3133_assigment1?retryWrites=true&w=majority";
+  const MONGODB_URI = "mongodb+srv://sarahrami97:rKcXzEa3p234lbya@cluster0.umsf83y.mongodb.net/comp3133_assigment1?retryWrites=true&w=majority";
 
-await mongoose.connect(MONGODB_URI)
-  .then(() => {
+  try {
+    await mongoose.connect(MONGODB_URI);
     console.log("Successfully connected to the database");
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log("Failed to connect to the database.", err);
     process.exit();
-  });
+  }
 
-const PORT = 4000;
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+  const PORT = 4000;
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
 
-app.listen(PORT, () =>
-  console.log(`Express GraphQL Server Now Running On http://localhost:${PORT}/graphql`)
-);
+  app.listen(PORT, () =>
+    console.log(`Express GraphQL Server Now Running On http://localhost:${PORT}/graphql`)
+  );
+}
+
+startServer().catch(err => console.error("Error starting server:", err));
