74195dcbe198c9ca8f3f44af139c1130b5a2ff9c
sarahrami <sarahrami97@gmail.com>
2024-04-07 00:02:22 -0400

    an

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-var { graphqlHTTP } = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const cors = require('cors');
-
 
 const schema = require("./graphql/Schema");
 const root = require("./graphql/resolvers");
@@ -13,24 +12,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
 
-// const DB_CONNECTION_STRING = process.env.MONGODB_URI;
-const MONGODB_URI="mongodb+srv://sarahrami97:rKcXzEa3p234lbya@cluster0.umsf83y.mongodb.net/comp3133_assigment1?retryWrites=true&w=majority"
+const MONGODB_URI = "mongodb+srv://sarahrami97:rKcXzEa3p234lbya@cluster0.umsf83y.mongodb.net/comp3133_assigment1?retryWrites=true&w=majority";
 
-mongoose
-  .connect(MONGODB_URI, {
-  })
+mongoose.connect(MONGODB_URI)
   .then(() => {
-    console.log("Successfully connected to database");
+    console.log("Successfully connected to the database");
   })
   .catch((err) => {
-    console.log("Failed to connect to database.", err);
+    console.log("Failed to connect to the database.", err);
     process.exit();
   });
-
-
 
 const PORT = 4000;
 app.use(
@@ -43,7 +36,5 @@ app.use(
 );
 
 app.listen(PORT, () =>
-  console.log(
-    `Express GraphQL Server Now Running On http://localhost:${PORT}/graphql`
-  )
+  console.log(`Express GraphQL Server Now Running On http://localhost:${PORT}/graphql`)
 );
